300e382f404b5c86706784ef01f58e34e8061997
Sükrü Yavuz <yavuz.suekrue@gmail.com>
2022-11-10 23:05:33 +0100

    selectedMitarbeiter ändern

--- src/app/home/admin/mitarbeiter/change-mitarbeiter.component.ts
+++ src/app/home/admin/mitarbeiter/change-mitarbeiter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/model/user';
+import { AdminService } from 'src/app/service/admin.service';
 
 @Component({
   selector: 'app-change-mitarbeiter',
@@ -10,10 +11,11 @@ import { User } from 'src/app/model/user';
 export class ChangeMitarbeiterComponent implements OnInit {
   @Input() selectedMitarbeiter?: User;
   changedMitarbeiter?: User;
+  clickedEdit:boolean = false;
 
   public form:FormGroup;
 
-  constructor(fb:FormBuilder) {
+  constructor(fb:FormBuilder, private adminService: AdminService) {
     this.form = fb.group({
       username:[null , Validators.required],
       forename:[null, Validators.required],
